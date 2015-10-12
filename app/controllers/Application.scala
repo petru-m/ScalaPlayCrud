8e4ba0d8aeb460f24d16ce3618357a0ecbcd1b0b
Petru Miftode <petru.miftode@assist.ro>
2015-10-12 18:03:03 +0300

    adding w2grid for data display

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -3,10 +3,11 @@ package controllers
 import play.api._
 import play.api.mvc._
 
+
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index())
   }
 
 }
--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -1,12 +1,14 @@
 package controllers
 
 import model.User
-import play.Logger
+import play.api.Logger
 import play.api.libs.json.Json
 import play.api.mvc.{Action, Controller}
 import dao.UserDAO
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
+
+
 
 /**
  * Created by petru on 08.10.2015.
@@ -16,12 +18,11 @@ class UserController  extends Controller{
   val dao = new UserDAO
   lazy implicit val userJson = Json.format[User]
 
-
   def getUsers=Action.async{implicit request=>
-    dao.all().map{user=>Ok(Json.toJson(user))}
+    dao.all().map{user=>
+      Ok(Json.toJson(Map("total" -> Json.toJson(user.length), "records" ->Json.toJson(user))))}
   }
   def saveUser = Action.async(parse.json){implicit  request=>
-    Logger.info(""+request.body.validate[User])
       request.body.validate[User].map{
       user=>dao.add(user).map(result=>Ok(Json.toJson(result)))
     }.getOrElse(Future.successful(BadRequest("invalid json")))
